626bf1026919aaecfd1b6b7b69597cc949a22f4d
unknown <reem.hasan98@yahoo.com>
2023-11-08 20:54:20 +0300

    try

---
+++ conn.php
@@ -0,0 +1,8 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname ="project-pre2";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
---
+++ filter/GETpriceRange.php
@@ -0,0 +1,36 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET');
+header('Content-Type: application/json; charset=UTF-8');
+include '../conn.php';
+if($_SERVER["REQUEST_METHOD"] == "GET"){
+if (isset($_GET['min_price']) || isset($_GET['max_price'])){
+    if(!isset ($_GET['min_price'])){
+        $max_price=$_GET['max_price'];
+        $sql="SELECT MIN(Price) FROM Products";
+        $result = $conn->query($sql);
+        $min=$result->fetch_assoc();
+        $min_price=$min['MIN(Price)'];
+        echo $min_price ;
+    }elseif(!isset ($_GET['max_price'])){
+        $min_price=$_GET['min_price'];
+        $sql="SELECT MAX(Price) FROM Products";
+        $min=$result->fetch_assoc();
+        $max_price=$min['MAX(Price)'];
+        echo $max_price ;
+    }else{
+        $max_price=$_GET['max_price'];
+        $min_price=$_GET['min_price'];
+
+    }
+    $sql="SELECT * FROM products WHERE Price BETWEEN '$min_price' AND '$max_price'";
+    
+
+$JSON=array();
+$result = $conn->query($sql);
+$row = mysqli_fetch_all($result, MYSQLI_ASSOC);
+$JSON=json_encode($row);
+ echo $JSON;}
+}else {
+    echo 'error method';
+}    
