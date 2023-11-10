fbb431257a983ea3a1ae38ba0dfae00a21837f1f
reem <144600489+ReemHasanA@users.noreply.github.com>
2023-11-10 15:30:47 +0300

    Delete conn.php

--- conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname ="project-pre2";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
