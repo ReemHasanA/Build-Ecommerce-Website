d929f76504dd6f97073608e4302c2527eaa963c6
ReemHasanA~ <reem.hasan98@yahoo.com>
2023-11-12 22:31:58 +0300

    n

--- Archive/ecommerce/loginSignUp/signup.js
+++ Archive/ecommerce/loginSignUp/signup.js
@@ -15,17 +15,17 @@ document.addEventListener("DOMContentLoaded", function () {
     const signIn = document.getElementById("signin");
     signIn.addEventListener("click", (event) => {
         event.preventDefault();
-        var username = document.getElementById("username").value;
+        var email = document.getElementById("email").value;
         var password = document.getElementById("password1").value;
-        console.log(username, password);
+        console.log(email, password);
 
-        var user = { username: username, password: password };
+        var user = { email: email, password: password };
         console.log(user);
 
         fetch("http://localhost/ecommerce/API1/user/login_oop.php", {
             method: "POST",
             body: JSON.stringify(user),
-            // headers: { "Content-Type": "application/json" }
+            headers: { "Content-Type": "application/json" }
         })
             .then((response) => {
                 if (response.ok) {
@@ -43,13 +43,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
                     
                     window.location.href = "../athath/index.html";
-                    sessionStorage.setItem("email", username);
+                    sessionStorage.setItem("isLoggedin", "true");
                 } else if (data.ROLE == 1 && data.STATUS === true ) {
-                    sessionStorage.setItem("email", username);
-                    window.location.href = "../admin/index.html";
+                    window.location.href = "../inbox.html";
                     console.log('Validation is false');
                 } else {
-                    alert("the username or password not valid")
+                    alert("the email or password not valid")
                 }
             })
             .catch((error) => {
@@ -59,66 +58,3 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-document.addEventListener("DOMContentLoaded", function () {
-    const signUpButton = document.getElementById('signUpForm');
-    const signInButton = document.getElementById('signin');
-    const container = document.getElementById('container');
-
-    signUpButton.addEventListener('click', () => {
-        container.classList.add("right-panel-active");
-    });
-
-    signInButton.addEventListener('click', () => {
-        container.classList.remove("right-panel-active");
-    });
-
-    const signUpForm = document.getElementById("signUpForm");
-    signUpForm.addEventListener("submit", function (event) {
-        event.preventDefault();
-
-        // Validate form inputs
-        const name = document.getElementById("name").value;
-        const email = document.getElementById("email").value;
-        const password = document.getElementById("password").value;
-        const confirmPassword = document.getElementById("checkPassword").value;
-
-        if (!name || !email || !password || !confirmPassword) {
-            alert("Please fill in all fields");
-            return;
-        }
-
-        if (password !== confirmPassword) {
-            alert("Passwords do not match");
-            return;
-        }
-
-        // If validation passes, proceed with API request
-        const user = { username: name, email: email, password: password };
-
-        fetch("http://localhost/ecommerce/API1/user/signup_api.php", {
-            method: "POST",
-            body: JSON.stringify(user),
-            headers: { "Content-Type": "application/json" },
-        })
-            .then((response) => {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    throw new Error('Network response was not ok');
-                }
-            })
-            .then((data) => {
-                if (data.status === 1) {
-                    alert('Registration successful');
-                    // Redirect or perform other actions after successful registration
-                } else if (data.status === 10) {
-                    alert('User with the same username already exists');
-                } else {
-                    alert('Registration failed: ' + data.message);
-                }
-            })
-            .catch((error) => {
-                console.error('Fetch error:', error);
-            });
-    });
-});
--- shop/conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname ="build-ecommerce-website-main";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
